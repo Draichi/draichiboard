315c2f03897d9fa344c37a9f2036e83dd035a4e8
draichi <lucasdraichi@gmail.com>
2020-07-31 23:12:45 -0300

    fix: adds build to production

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
-    publicPath: '/draichiboard/'
+    publicPath: 'https://draichi.github.io/draichiboard/'
   },
   generate: {
     dir: 'docs'
