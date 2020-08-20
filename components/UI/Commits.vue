3f1121e40aa630cd6a575e27f173dab9e56a916e
draichi <lucasdraichi@gmail.com>
2020-08-20 03:17:52 -0300

    refactor: line graph color

--- components/UI/Commits.vue
+++ components/UI/Commits.vue
@@ -3,7 +3,7 @@
     <div style="height: 10%;">
       <h2 class="font-xl text-white p-4">Commits</h2>
     </div>
-    <LineChart class="h-auto" style="height: 90%;" />
+    <LineChart :data="data" class="h-auto" style="height: 90%;" />
   </div>
 </template>
 
@@ -15,7 +15,31 @@ import { Vue, Component } from 'vue-property-decorator'
     LineChart: () => import('@/components/Charts/Line.js'),
   },
 })
-export default class CommitsChart extends Vue {}
+export default class CommitsChart extends Vue {
+  data = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ],
+    datasets: [
+      {
+        label: 'GitHub Commits',
+        backgroundColor: '#f87979',
+        data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+      },
+    ],
+  }
+}
 </script>
 
 <style lang="scss" scoped>
