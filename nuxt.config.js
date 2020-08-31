7bc16f8e45ddfd8aa4391151d71b96e3afb81e9a
draichi <lucasdraichi@gmail.com>
2020-08-30 23:58:52 -0300

    adds favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,14 +15,15 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Draichiboard',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon-16x16.png' },
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
