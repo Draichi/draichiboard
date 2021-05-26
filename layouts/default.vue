2254fba5475b5786352a114f294bda1e0c0ff967
draichi <lucasdraichi@gmail.com>
2021-05-26 03:00:19 -0300

    fix: portfolio background

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,15 +6,9 @@
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  background: rgb(24, 24, 38);
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
