4ad6eca0d7e5f5a1f70ecc89d17a70c29bc1dd13
@Draichi <lucasdraichi@gmail.com>
2023-01-08 01:46:46 -0300

    wip: HeroCard component

--- components/HeroCard.vue
+++ components/HeroCard.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Lucas Draichi</h1>
+  <div :class="$style['hero-card']">
+    <h1 :class="$style['hero-card__text']">Lucas Draichi</h1>
+  </div>
 </template>
+
+<style module>
+.hero-card {
+  background-color: #111518;
+  border-radius: 20px;
+  padding: 1.625rem 0;
+}
+
+.hero-card__text {
+  color: #36a2eb;
+  text-align: center;
+  font-family: fantasy;
+}
+</style>
