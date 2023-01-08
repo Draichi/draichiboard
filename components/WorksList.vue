4418135ab76235bb151f8e78012a5c9e1d4992a1
@Draichi <lucasdraichi@gmail.com>
2023-01-08 01:40:43 -0300

    wip: WorksList component

--- components/WorksList.vue
+++ components/WorksList.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul>
+  <ul :class="$style.works">
     <li>IBM</li>
     <li>Sabido</li>
     <li>Globo</li>
@@ -7,3 +7,16 @@
     <li>Age of Learning</li>
   </ul>
 </template>
+
+<style module>
+.works {
+  background-color: #181711;
+  color: #ebeae6;
+  font-size: 2.75rem;
+  border-radius: 20px;
+  display: flex;
+  gap: 3.75rem;
+  flex-direction: column;
+  padding: 3.875rem 0.8125rem;
+}
+</style>
