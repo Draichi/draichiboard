11252b0b7204cd70318cfc3bfa2e38c92f73b42a
@Draichi <lucasdraichi@gmail.com>
2023-01-13 03:08:16 -0300

    feat: add google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,23 @@ import viteEslintPlugin from 'vite-plugin-eslint'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Draichiboard',
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: '',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Lacquer&family=Poppins:wght@400;500&display=swap',
+        },
+      ],
+    },
+  },
   vite: {
     plugins: [
       {
