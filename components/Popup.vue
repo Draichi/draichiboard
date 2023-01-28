64afb05144a0e2610356569d368cd8f1b661de1b
@Draichi <lucasdraichi@gmail.com>
2023-01-28 16:56:12 -0300

    wip(home): create Popup component

---
+++ components/Popup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <h3>IBM</h3>
+      <button @click="$emit('close-button:click')">X</button>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<style module>
+.container {
+  background-color: #181711;
+  color: #ebeae6;
+  border-radius: 20px;
+}
+</style>
