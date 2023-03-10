72ce8703206f2e5624b469e73bf91f7d4d4f2b19
@Draichi <lucasdraichi@gmail.com>
2023-03-10 00:54:35 -0300

    fix: delete trackRouter

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,14 +1,17 @@
-import VueGTag, { trackRouter } from 'vue-gtag'
+import VueGTag from 'vue-gtag'
 import { defineNuxtPlugin, useRouter } from '#imports'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueGTag, {
-    config: {
-      id: 'G-PFFDM9Q014',
-      params: {
-        send_page_view: true,
+  nuxtApp.vueApp.use(
+    VueGTag,
+    {
+      config: {
+        id: 'G-PFFDM9Q014',
+        params: {
+          send_page_view: true,
+        },
       },
     },
-  })
-  trackRouter(useRouter())
+    useRouter()
+  )
 })
