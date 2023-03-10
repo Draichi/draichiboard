b375c17aaadebe442e83e854d4f24d6d67ad1709
@Draichi <lucasdraichi@gmail.com>
2023-03-10 00:22:23 -0300

    wip: gtag property

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -5,6 +5,12 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGTag, {
     config: {
       id: 'G-PFFDM9Q014',
+      params: {
+        send_page_view: true,
+      },
+    },
+    property: {
+      id: 'G-PFFDM9Q014',
     },
   })
 })
