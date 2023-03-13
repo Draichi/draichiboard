0392709d3aee29effc92a2d6c542d4e8988c8f30
@Draichi <lucasdraichi@gmail.com>
2023-03-13 00:47:51 -0300

    chore: use dotenv with analytics key

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,18 +1,20 @@
 import VueGTag from 'vue-gtag'
 import { defineNuxtPlugin, useRouter } from '#imports'
 
+require('dotenv').config()
+
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(
     VueGTag,
     {
       config: {
-        id: 'G-PFFDM9Q014',
+        id: process.env.ANALYTICS_ID,
         params: {
           send_page_view: true,
         },
       },
       property: {
-        id: 'G-PFFDM9Q014',
+        id: process.env.ANALYTICS_ID,
       },
     },
     useRouter()
