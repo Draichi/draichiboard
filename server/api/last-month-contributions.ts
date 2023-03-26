428ae09f137236f68362d45c3368295f9576b1ab
@Draichi <lucasdraichi@gmail.com>
2023-03-26 20:25:02 -0300

    feat(dashboard): create last-month-contirbutions API

---
+++ server/api/last-month-contributions.ts
@@ -0,0 +1,34 @@
+import { fetchContributionsCollectionAPI } from '@/server/utils'
+
+const query = `
+  query ($login: String!, $from: DateTime, $to: DateTime) {
+    user(login: $login) {
+      contributionsCollection(from: $from, to: $to) {
+        contributionCalendar {
+          weeks {
+            contributionDays {
+              contributionCount
+              date
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const now = new Date()
+const to = new Date()
+const from = new Date(now.setDate(now.getDate() - 30))
+
+export default defineEventHandler(async () => {
+  const res = await fetchContributionsCollectionAPI(from, to, query)
+
+  const foo = res?.contributionsCollection.contributionCalendar.weeks
+    .flatMap((week) => week.contributionDays)
+    .flatMap((contributionDay) => contributionDay)
+
+  return {
+    foo,
+  }
+})
